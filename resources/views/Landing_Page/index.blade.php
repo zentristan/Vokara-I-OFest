8e742e094e7595305bba71ca5ab3de035c4d9d17
Zentristan vitadi <zentristan@Zentristans-MacBook-Air.local>
2026-04-15 15:22:22 +0700

    testing landing

--- resources/views/Landing_Page/index.blade.php
+++ resources/views/Landing_Page/index.blade.php
@@ -4,8 +4,30 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Vokara</title>
+
+    <!-- Jakarta Plus Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+
+    <!-- Outfit Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+
+    <!-- Cormant Garamound -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+
+    <!-- FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/ef1f748698.js" crossorigin="anonymous"></script>
+
+    <!-- CONNECTING CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{ asset('css/LandingPage.css') }}">
 </head>
 
 <body>
@@ -16,8 +38,8 @@
 
                 </div>
                 <div class="nav-menu">
-                    <a href="#">Lowongan Jasa</a>
-                    <a href="#">Jelajahi Jasa</a>
+                    <a href="#" class="nav-link">Jelajahi Jasa</a>
+                    <a href="#" class="nav-link">Cari Talenta</a>
                 </div>
                 <div class="hamburger" id="hamburger">
                     <span></span>
@@ -28,7 +50,35 @@
         </div>
     </nav>
 
-    
+    <section class="hero">
+        <div class="container">
+
+        </div>
+    </section>
+
+    <section class="tentang-kami">
+        <div class="container">
+
+        </div>
+    </section>
+
+    <section class="keahlian">
+        <div class="container">
+            
+        </div>
+    </section>
+
+    <section class="testimonials">
+        <div class="container">
+
+        </div>
+    </section>
+
+    <section class="starting">
+        <div class="container">
+
+        </div>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
